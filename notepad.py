a1a9bfa5251ef033ec048848b8d0d542871c848d
ArkOfReis <70862343+ArkOfReis@users.noreply.github.com>
2021-02-19 09:26:21 +0100

    Notepad Script

---
+++ notepad.py
@@ -0,0 +1,74 @@
+import PySimpleGUI as sg
+import pathlib
+
+sg.ChangeLookAndFeel('BrownBlue')
+
+WIN_W = 90
+WIN_H = 25
+file = None
+
+menu_layout = [['File', ['New (Ctrl+N)', 'Open (Ctrl+O)', 'Save (Ctrl+S)', 'Save As', '---', 'Exit']],
+              ['Tools', ['Word Count']],
+              ['Help', ['About']]]
+
+layout = [[sg.Menu(menu_layout)],
+          [sg.Text('> untitled <', font=('Consolas', 10), size=(WIN_W, 1), key='_INFO_')],
+          [sg.Multiline(font=('Consolas', 12), size=(WIN_W, WIN_H), key='_BODY_')]]
+
+window = sg.Window('Python Notepad', layout=layout, margins=(0, 0), resizable=True, return_keyboard_events=True, finalize=True)
+window.maximize()
+window['_BODY_'].expand(expand_x=True, expand_y=True)
+
+def new_file():
+    window['_BODY_'].update(value='')
+    window['_INFO_'].update(value='> New File <')
+    file = None
+    return file
+
+def open_file():
+    filename = sg.popup_get_file('Open', no_window=True)
+    if filename:
+        file = pathlib.Path(filename)
+        window['_BODY_'].update(value=file.read_text())
+        window['_INFO_'].update(value=file.absolute())
+        return file
+
+
+def save_file(file):
+    if file:
+        file.write_text(values.get('_BODY_'))
+    else:
+        save_file_as()
+
+def save_file_as():
+    filename = sg.popup_get_file('Save As', save_as=True, no_window=True)
+    if filename:
+        file = pathlib.Path(filename)
+        file.write_text(values.get('_BODY_'))
+        window['_INFO_'].update(value=file.absolute())
+        return file
+
+def word_count():
+    words = [w for w in values['_BODY_'].split(' ') if w!='\n']
+    word_count = len(words)
+    sg.popup_no_wait('Word Count: {:,d}'.format(word_count))
+
+def about_me():
+    sg.popup_no_wait('"All great things have small beginnings" - Peter Senge')
+
+while True:
+    event, values = window.read()
+    if event in('Exit', None):
+        break
+    if event in ('New (Ctrl+N)', 'n:78'):
+        file = new_file()
+    if event in ('Open (Ctrl+O)', 'o:79'):
+        file = open_file()
+    if event in ('Save (Ctrl+S)', 's:83'):
+        save_file(file)
+    if event in ('Save As',):
+        file = save_file_as()   
+    if event in ('Word Count',):
+        word_count() 
+    if event in ('About',):
+        about_me()
